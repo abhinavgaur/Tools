93a843b51f15747730a498d6deb366655c5b05bf
zhuth <zthpublic@gmail.com>
2012-08-30 19:17:51 +0800

    ImportToMysql fixed; scripts added.

--- matlab/gmeans.m
+++ matlab/gmeans.m
@@ -1,10 +1,11 @@
-function [L C] = gmeans(data, alpha)
+function [L C] = gmeans(data, maxk, alpha)
 %1
-if (nargin < 2), alpha = 0; end;
+if (nargin < 2), maxk = size(data, 1) / 8; end;
+if (nargin < 3), alpha = 0; end;
 n = size(data, 1);
 nC = 1;
 C(1,:) = mean(data);
-while 1,
+while nC <= maxk,
     %2
     incr_C = 0;
     [L C] = kmeans(data, nC, 'start', C, 'EmptyAction', 'singleton');
@@ -25,20 +26,7 @@ while 1,
 end;
 
 function [t CC]=look_gaussian(data, alpha, c)
-
-if (alpha == 0), alpha = 0.005; end;
-switch alpha
-    case 0.1
-        alpha = 0.631;
-    case 0.05
-        alpha = 0.752;
-    case 0.025
-        alpha = 0.873;
-    case 0.01
-        alpha = 1.035;
-    case 0.005
-        alpha = 1.159;
-end;
+if (alpha == 0), alpha = 0.05; end;
 
 if (size(data,1) == 1)
     t = 1; CC = [data; data];
@@ -48,7 +36,6 @@ end;
 % choose two new centers, step 2
 [~, lambda, s] = svds(data, 1);
 m = s' * sqrt(2 * lambda / pi);
-%disp(size(m)); disp(size(c));
 CC(1, :) = c + m; CC(2, :) = c - m;
 
 %3
@@ -57,17 +44,45 @@ v = CC(1, :) - CC(2, :);
 
 %4
 nd = size(data, 1);
-%%disp('data'); disp(size(data)); disp('v'); disp(size(v));
-X = data*v';
-X = X / std(X); X = X - mean(X);
+X = data*v' / norm(v);
+X = (X - mean(X)) / std(X);
 %5
-Z = normcdf(X, 0, 1);
+t = AnDartest(X, alpha);
 
-% compute A^2(Z)
-AA = 0;
-for j=1:nd,
-    AA = AA + (2*j-1)*(log(Z(j)) + log(1 - Z(nd+1-j)));
-end;
-AA = - AA / nd + nd;
-AA = AA * (1+4/nd-25/(nd*nd)); %A-star^2(Z)
-t = (AA <= alpha);
+function [test] = AnDartest(x,alpha)
+n = length(x);
+if n < 3,
+    test = 1;
+    return,
+else
+    x = x(:);
+    x = sort(x);
+    fx = normcdf(x,mean(x),std(x));
+    i = 1:n;
+
+    S = sum((((2*i)-1)/n)*(log(fx)+log(1-fx(n+1-i))));
+    AD2 = -n-S;
+
+    AD2a = AD2*(1 + 0.75/n + 2.25/n^2); %correction factor for small sample sizes: case normal
+
+    if (AD2a >= 0.00 && AD2a < 0.200);
+        P = 1 - exp(-13.436 + 101.14*AD2a - 223.73*AD2a^2);
+    elseif (AD2a >= 0.200 && AD2a < 0.340);
+        P = 1 - exp(-8.318 + 42.796*AD2a - 59.938*AD2a^2);
+    elseif (AD2a >= 0.340 && AD2a < 0.600);
+        P = exp(0.9177 - 4.279*AD2a - 1.38*AD2a^2);
+    else (AD2a >= 0.600 && AD2a <= 13);
+        P = exp(1.2937 - 5.709*AD2a + 0.0186*AD2a^2);
+    end
+end
+
+if P >= alpha;
+   test=1;
+else
+   test=0;
+end
+
+return,
+
+
+
