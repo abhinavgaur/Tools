784e0f2bbe83fc4183d946354ce2e45503a2ad80
zhuth <zlic@gmail.com>
2012-09-02 19:01:34 +0800

    dict, wordseg, cssc64 and mysqlconn update

--- MySqlConnector/Main.cs
+++ MySqlConnector/Main.cs
@@ -49,53 +49,59 @@ namespace MySqlConnector
 
 		public static void Main (string[] args)
 		{
-			if (args.Length < 2) {
-				Console.WriteLine ("Usage: importToMySql\t<db name> <filename> [table_name]");
-				return;
-			}
+            string[] list = System.IO.Directory.GetFiles(args[0]);
+            string cs = @"server=localhost;userid=root;password=F8e2B5V4a;Charset=utf8;database=stockdata";
+            conn = new MySqlConnection(cs);
+            conn.Open();
+            Console.WriteLine("MySQL version : {0}", conn.ServerVersion);
+            ExecuteSql("set names utf8;");
+             
+            foreach (string filename in list)
+            {
+                string table_name = filename.Substring(filename.LastIndexOf(System.IO.Path.DirectorySeparatorChar) + 1);
+                table_name = table_name.Substring(0, table_name.IndexOf('.'));
+             
+                //try {
+                ExecuteSql("DROP TABLE IF EXISTS `" + table_name + "`");
+                ExecuteSql("CREATE TABLE IF NOT EXISTS `" + table_name + "`("
+                           + "`id` INT PRIMARY KEY AUTO_INCREMENT, "
+                           + "`date` VARCHAR(12), `title` VARCHAR(255), `content` TEXT"
+                           + ") ENGINE=MyISAM default charset utf8;");
 
-			string filename = args [1];
-            string cs = @"server=localhost;userid=root;password=;Charset=utf8;database=" + args[0];
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(filename))
+                {
+                    int count = 0;
+                    while (true)
+                    {
+                        string line = sr.ReadLine();
+                        if (line == null) break;
+                        if (++count % 50 == 0) Console.WriteLine("Importing from line {0}...", count);
+                        string[] cols = line.Split('\t');
+                        if (cols.Length < 3) continue;
+                        if (string.IsNullOrEmpty(cols[2])) continue;
 
-			string table_name = filename.Substring (filename.LastIndexOf(System.IO.Path.DirectorySeparatorChar) + 1);
-			table_name = table_name.Substring (0, table_name.IndexOf ('.'));
-			if (args.Length >= 3) table_name = args [2];
+                        for (int i = 0; i < cols.Length; ++i)
+                            cols[i] = cols[i].Replace("\'", "\'\'");
 
-			//try {
-				conn = new MySqlConnection (cs);
-				conn.Open ();
-				Console.WriteLine ("MySQL version : {0}", conn.ServerVersion);
-
-				ExecuteSql("DROP TABLE IF EXISTS `" + table_name + "`");
-				ExecuteSql("CREATE TABLE IF NOT EXISTS `" + table_name + "`("
-				           + "`id` INT PRIMARY KEY AUTO_INCREMENT, "
-				           + "`date` VARCHAR(8), `title` VARCHAR(255), `content` TEXT"
-				           + ") ENGINE=MyISAM;");
-
-				using(System.IO.StreamReader sr = new System.IO.StreamReader(filename)) {
-					int count = 0;
-					while (true) {
-						if (count % 50 == 0) Console.WriteLine("Importing from line {0}...", ++count);
-						string s = sr.ReadLine();
-						if (s == null) break;
-
-						string[] cols = s.Split('\t');
-						if (cols.Length == 3) {
-							for (int i = 0; i < cols.Length; ++i)
-								cols[i] = cols[i].Replace("\'", "\'\'");
-
-							ExecuteSql("INSERT INTO `" + table_name + "`(`date`, `title`, `content`) VALUES('" + cols[0] + "','" + cols[1] + "','" + cols[2] + "')");
-						}
-					}
-				}
-			/*
-            } catch (MySqlException ex) {
-				Console.WriteLine ("Error: {0}", ex.ToString ());
-			} finally {          
-				if (conn != null) {
-					conn.Close ();
-				}
-			}*/
+                        //try
+                        {
+                            ExecuteSql("INSERT INTO `" + table_name + "`(`date`, `title`, `content`) VALUES('" + cols[0] + "','" + cols[1] + "','" + cols[2] + "')");
+                        }
+                        //catch (MySqlException ex)
+                        {
+                        //    Console.WriteLine("Record {1}, Exception: {0}", ex, count);
+                        }
+                    }
+                }
+                /*
+                } catch (MySqlException ex) {
+                    Console.WriteLine ("Error: {0}", ex.ToString ());
+                } finally {          
+                    if (conn != null) {
+                        conn.Close ();
+                    }
+                }*/
+            }
 	    }
 	}
 }
